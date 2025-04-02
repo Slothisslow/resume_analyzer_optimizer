7dde64f190e7f26278ee95cb3a30972fa1730179
ajay17407 <ajaykanna17407@gmail.com>
2025-04-02 19:21:46 +0530

    Initial setup of backend and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl">
+      Tailwind via Vite Plugin! 🚀
+    </div>
+  )
+}
+
+export default App
